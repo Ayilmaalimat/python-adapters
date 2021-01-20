ec05ce1133fc9aa7183c3a785e0bee1fbb16a7f0
Abdisattarov Jetigen <jetigeenn@gmail.com>
2021-01-20 19:52:36 +0900

    delete sys args

--- webclient.py
+++ webclient.py
@@ -1,13 +1,13 @@
 import base64
-import sys
 from PIL import Image
 from io import BytesIO
+
 from suds.client import Client
 
 c = Client('http://127.0.0.1:8000/Producer?wsdl')
-data = c.service.XRoad(sys.argv[1])
-image = c.service.ImageDownload(sys.argv[1])
+data = c.service.XRoad(1000)
+image = c.service.ImageDownload(1000)
 
 
 im = Image.open(BytesIO(base64.b64decode(image)))
-im.save(f'image{sys.argv[1]}.png', 'PNG')
+im.save(f'image{1000}.png', 'PNG')
